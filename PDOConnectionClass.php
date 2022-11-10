fa4c11143726db074e595e500b5dbf1011f60537
daria0805 <dariabobitan@yahoo.com>
2022-11-10 16:00:23 +0200

    commit_pdo

---
+++ PDOConnectionClass.php
@@ -0,0 +1,59 @@
+<?php
+
+class PDOConnectionClass
+{
+
+    private $host_name;
+    private $dbname;
+    private $username;
+    private $password;
+    private $conn;
+
+    public function connect()
+    {
+        $this->host_name = "localhost";
+        $this->dbname = "mydatabase2";
+        $this->username = "root";
+        $this->password = "";
+
+        try {
+            $this->conn = new PDO("mysql:host=$this->host_name;dbname=$this->dbname", $this->username, $this->password);
+
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo 'connection succesfully';
+        } catch (PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+    }
+
+    public function get(string $tableName):array
+    {
+        $query ="SELECT * FROM . $tableName";
+        $result =$this->conn->query($query);
+        return $result->fetchAll();
+    }
+
+    public function update(string $tableName, array $updateData):bool
+    {
+        $query="UPDATE $tableName SET `email`='$updateData[0]' WHERE `id_cus`=$updateData[1]";
+        $result=$this->conn->query($query);
+        return(bool) $result->rowCount();
+    }
+
+    public function insert(string $tableName, array $insertData):bool
+    {
+        $query="INSERT INTO $tableName (`email`) VALUES ('$insertData[0]')";
+        $result=$this->conn->query($query);
+        
+        return $this->conn->lastInsertId();
+    }
+
+    public function delete($tableName, int $recordId):bool
+    {
+        $query="DELETE FROM $tableName WHERE `id_cus`=$recordId";
+        $result=$this->conn->query($query);
+        
+        return(bool) $result->rowCount();
+    }
+
+}
--- index.php
+++ index.php
@@ -1,11 +1,45 @@
 <?php
 
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once('ClassProduct.php');
 require_once('ProductController.php');
+require_once('PDOConnectionClass.php');
+
+$conn=new PDOConnectionClass();
+$conn->connect();
+//$customer=$conn->get('customer');
+//$customerUpdatedState=$conn->update('customer', ['nest@yahoo', 1]);
+//$insert_customer=$conn->insert('customer', ['nesting@yahoo']);
+$deleteCustomer=$conn->delete('customer', 1);
+// echo'<pre>';
+// var_dump($insert_customer);
+// echo '</pre>';
+
+// echo'<pre>';
+// var_dump($customerUpdatedState);
+// echo '</pre>';
+
+// echo'<pre>';
+// var_dump($customer);
+// echo '</pre>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //echo 'index.php';
 
 // $productsObj= new Product();
@@ -17,8 +51,7 @@ require_once('ProductController.php');
 //$price=$productsObj->getPrice();
 //echo $price;
 
-$productsControllerObj=new ProductController(100,'frigider');
+//$productsControllerObj=new ProductController(100,'frigider');
 // $productsControllerObj->manageProduct(11, 'prod1');
 // $productsControllerObj->manageProduct(12, 'prod2');
 // $productsControllerObj->manageProduct(13, 'prod3');
-?>
